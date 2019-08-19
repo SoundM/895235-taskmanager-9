39bb3d5c609156f929c48c7b84949e3201f39806
SoundM <galina45192@gmail.com>
2019-08-19 23:01:02 +0200

    Моки для карточек

---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const getTask = () => ({
+  description: [
+    `Изучить теорию`,
+    `Сделать домашку`,
+    `Пройти интенсив на соточку`,
+    `создать резюме`,
+    `не забыть сходить на работу`,
+  ][Math.floor(Math.random() * 5)],
+  dueDate: Date.now() + 1 + Math.floor(Math.random() * 7) * 24 * 60 * 60 * 1000,
+  tags: new Set([
+    `homework`,
+    `theory`,
+    `practice`,
+    `intensive`,
+    `keks`
+  ]),
+  repeatingDays: {
+    'mo': false,
+    'tu': false,
+    'we': Boolean(Math.round(Math.random())),
+    'th': false,
+    'fr': false,
+    'sa': false,
+    'su': false,
+  },
+  color: [
+    `black`,
+    `yellow`,
+    `blue`,
+    `green`,
+    `pink`,
+  ][Math.floor(Math.random() * 5)],
+  isFavorite: Boolean(Math.round(Math.random())),
+  isArchive: Boolean(Math.round(Math.random())),
+});
--- src/main.js
+++ src/main.js
@@ -3,35 +3,38 @@ import {createTemplateSearch} from './components/search.js';
 import {createTemplateFilter} from './components/filter.js';
 import {createTemplateBoardFilter} from './components/board.js';
 import {createTemplateBoardContainer} from './components/board-container.js';
-import {createTaskCard} from './components/card.js';
+import {createTemplateCard} from './components/card.js';
 import {createTemplateButton} from './components/button.js';
+import {getTask} from '../src/data.js';
 
-const CARD_COUNT = 3;
+const CARD_COUNT = 5;
+const EXTRA_CARD_COUNT = 8;
 const containerForMain = `.main`;
 const containerForMenu = `.main__control`;
 const board = `.board`;
 const boardContainer = `.board__tasks`;
+
 
 const elements = [
   {
     container: containerForMenu,
     template: createTemplateMenu,
     place: `beforeEnd`,
-    amount: 1
+    amount: 1,
   },
 
   {
     container: containerForMain,
     template: createTemplateSearch,
     place: `beforeEnd`,
-    amount: 1
+    amount: 1,
   },
 
   {
     container: containerForMain,
     template: createTemplateFilter,
     place: `beforeEnd`,
-    amount: 1
+    amount: 1,
   },
 
   {
@@ -45,22 +48,15 @@ const elements = [
     container: board,
     template: createTemplateBoardFilter,
     place: `afterBegin`,
-    amount: 1
+    amount: 1,
   },
 
   {
     container: board,
     template: createTemplateButton,
     place: `beforeEnd`,
-    amount: 1
+    amount: 1,
   },
-
-  {
-    container: boardContainer,
-    template: createTaskCard,
-    place: `beforeEnd`,
-    amount: CARD_COUNT
-  }
 ];
 
 const renderAllComponents = () => {
@@ -72,3 +68,25 @@ const renderAllComponents = () => {
   });
 };
 renderAllComponents();
+
+const card = {
+  container: boardContainer,
+  template: createTemplateCard,
+  place: `beforeEnd`,
+  task: getTask,
+};
+
+const renderCards = (amount) => {
+  const currentContainer = document.querySelector(card.container);
+  for (let i = 0; i < amount; i++) {
+    currentContainer.insertAdjacentHTML(card.place, card.template(card.task()));
+  }
+};
+
+renderCards(CARD_COUNT);
+
+const buttonLoadMore = document.querySelector(`.load-more`);
+
+buttonLoadMore.addEventListener(`click`, () => {
+  renderCards(EXTRA_CARD_COUNT);
+});
